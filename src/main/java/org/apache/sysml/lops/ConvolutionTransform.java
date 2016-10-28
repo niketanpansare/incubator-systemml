7d0a1e271afda126a52a66bdf8f6dcd1824177c1
Niketan Pansare <npansar@us.ibm.com>
2016-10-28 10:56:06 -0700

    [SYSTEMML-446] Added non-update inplace bias_add, sel+ and removed redirecting of sparse GPU convolution instructions to CP.
    Since more careful investigation of bias_add API is necessary, I am not
    changing the NN library in this case.

--- src/main/java/org/apache/sysml/lops/ConvolutionTransform.java
+++ src/main/java/org/apache/sysml/lops/ConvolutionTransform.java
@@ -31,7 +31,8 @@ public class ConvolutionTransform extends Lop
 	
 	public enum OperationTypes {
 		MAX_POOLING, MAX_POOLING_BACKWARD,
-		DIRECT_CONV2D, DIRECT_CONV2D_BACKWARD_FILTER, DIRECT_CONV2D_BACKWARD_DATA
+		DIRECT_CONV2D, DIRECT_CONV2D_BACKWARD_FILTER, DIRECT_CONV2D_BACKWARD_DATA,
+		BIAS_ADD
 	};
 	
 	private OperationTypes operation = null;
@@ -105,6 +106,9 @@ public class ConvolutionTransform extends Lop
 		
 		case DIRECT_CONV2D:
 			return "conv2d";
+		
+		case BIAS_ADD:
+			return "bias_add";
 			
 		case DIRECT_CONV2D_BACKWARD_FILTER:
 			return "conv2d_backward_filter";
@@ -115,6 +119,33 @@ public class ConvolutionTransform extends Lop
 		default:
 			throw new UnsupportedOperationException(this.printErrorLocation() + "Instruction is not defined for Transform operation " + operation);
 				
+		}
+	}
+	
+	public String getInstructions(String input, String bias, String output) throws LopsException {
+		if(operation == OperationTypes.BIAS_ADD) {
+			StringBuilder sb = new StringBuilder();
+			sb.append( getExecType() );
+			
+			sb.append( OPERAND_DELIMITOR );
+			sb.append( getOpcode() );
+			sb.append( OPERAND_DELIMITOR );
+			sb.append( getInputs().get(0).prepInputOperand(input));
+			sb.append( OPERAND_DELIMITOR );
+			sb.append( getInputs().get(0).prepInputOperand(bias));
+			//output
+			sb.append( OPERAND_DELIMITOR );
+			sb.append( this.prepOutputOperand(output));
+			
+			//append degree of parallelism
+			if( getExecType()==ExecType.CP ) {
+				sb.append( OPERAND_DELIMITOR );
+				sb.append( numThreads );
+			}
+			return sb.toString();
+		}
+		else {
+			throw new LopsException("The operation is not supported with two operands:" + operation.name());
 		}
 	}
 	
