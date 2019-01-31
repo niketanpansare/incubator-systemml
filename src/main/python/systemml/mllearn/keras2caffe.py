ea862affc821ded1ba44595564d579e4cc601f12
Niketan Pansare <npansar@us.ibm.com>
2019-01-31 15:12:08 -0800

    bugfix for newer keras version

--- src/main/python/systemml/mllearn/keras2caffe.py
+++ src/main/python/systemml/mllearn/keras2caffe.py
@@ -333,6 +333,9 @@ def convertKerasToCaffeNetwork(
     _checkIfValid(kerasModel.layers, lambda layer: False if type(
         layer) in supportedLayers else True, 'Unsupported Layers:')
     with open(outCaffeNetworkFilePath, 'w') as f:
+        # Append an Input layer if not present: required for versions 2.1.5 (works with 2.1.5, but not with 2.2.4)
+        if not any([isinstance(l, keras.layers.InputLayer) for l in kerasModel.layers]):
+            _appendKerasLayers(f, keras.layers.InputLayer(input_shape=kerasModel.layers[0].input_shape), batch_size)
         # Write the parsed layers for all but the last layer
         _appendKerasLayers(f, kerasModel.layers[:-1], batch_size)
         # Now process the last layer with loss
