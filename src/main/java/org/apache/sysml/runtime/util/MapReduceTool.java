dcdd4de077cc761dd19137d150baac3166f6b643
Niketan Pansare <npansar@us.ibm.com>
2019-03-19 09:56:51 -0700

    skip writing of empty mtd path

--- src/main/java/org/apache/sysml/runtime/util/MapReduceTool.java
+++ src/main/java/org/apache/sysml/runtime/util/MapReduceTool.java
@@ -423,14 +423,16 @@ public class MapReduceTool
 	{
 		Path path = new Path(mtdfile);
 		if(path.getName().equals(" .mtd")) {
-			throw new IOException("Unsupported write of empty path");
+			LOG.warn("Skipping the write of empty mtd path:" + mtdfile);
 		}
-		FileSystem fs = IOUtilFunctions.getFileSystem(path);
-		try( BufferedWriter br = new BufferedWriter(new OutputStreamWriter(fs.create(path,true))) ) {
-			String mtd = metaDataToString(vt, schema, dt, mc, outinfo, formatProperties);
-			br.write(mtd);
-		} catch (Exception e) {
-			throw new IOException("Error creating and writing metadata JSON file", e);
+		else {
+			FileSystem fs = IOUtilFunctions.getFileSystem(path);
+			try( BufferedWriter br = new BufferedWriter(new OutputStreamWriter(fs.create(path,true))) ) {
+				String mtd = metaDataToString(vt, schema, dt, mc, outinfo, formatProperties);
+				br.write(mtd);
+			} catch (Exception e) {
+				throw new IOException("Error creating and writing metadata JSON file", e);
+			}
 		}
 	}
 
