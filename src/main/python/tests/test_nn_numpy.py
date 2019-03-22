cd835e59915e40bda95c01526783d1d4dc36176d
Niketan Pansare <npansar@us.ibm.com>
2019-03-22 13:54:54 -0700

    bugfix

--- src/main/python/tests/test_nn_numpy.py
+++ src/main/python/tests/test_nn_numpy.py
@@ -280,19 +280,19 @@ class TestNNLibrary(unittest.TestCase):
         self.failUnless(test_forward(ZeroPadding2D(padding=1, input_shape=(3, 64, 32))))
 
     def test_zeropadding_backward(self):
-        self.failUnless(test_backward(ZeroPadding2D(size=1, input_shape=(3, 64, 32))))
+        self.failUnless(test_backward(ZeroPadding2D(padding=1, input_shape=(3, 64, 32))))
 
     def test_zeropadding_forward1(self):
         self.failUnless(test_forward(ZeroPadding2D(padding=(1, 2), input_shape=(3, 64, 32))))
 
     def test_zeropadding_backward1(self):
-        self.failUnless(test_backward(ZeroPadding2D(size=(1, 2), input_shape=(3, 64, 32))))
+        self.failUnless(test_backward(ZeroPadding2D(padding=(1, 2), input_shape=(3, 64, 32))))
 
     def test_zeropadding_forward2(self):
         self.failUnless(test_forward(ZeroPadding2D(padding=((3, 2), (1, 3)), input_shape=(3, 64, 32))))
 
     def test_zeropadding_backward2(self):
-        self.failUnless(test_backward(ZeroPadding2D(size=((3, 2), (1, 3)), input_shape=(3, 64, 32))))
+        self.failUnless(test_backward(ZeroPadding2D(padding=((3, 2), (1, 3)), input_shape=(3, 64, 32))))
 
 if __name__ == '__main__':
     unittest.main()
